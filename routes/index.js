44da8d7443c3ddf45599a0f270777a4df6916125
siraphop-oLe <siraphop.rmutt@gmail.com>
2025-04-03 23:01:14 +0700

    update autoroutes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,27 @@
 import { Router } from "express"
-import notifyRoute from "./notify.js"
+import fs from "fs"
+import path from "path"
 
 const router = Router()
+const __dirname = path.resolve()
 
-// root route
+const routesPath = path.join(__dirname, "routes")
+
+fs.readdirSync(routesPath).forEach(async (file) => {
+    const fileName = path.parse(file).name
+    const fileExt = path.parse(file).ext
+
+    if (fileName === "index" || fileExt !== ".js") return
+
+    const route = await import(`./${file}`)
+    router.use(`/${fileName}`, route.default)
+})
+
+// Root route
 router.get("/", (req, res) => {
     res.status(200).json({
         message: "Welcome to the Telegram Bot API",
     })
 })
-
-// other routes
-router.use("/notify", notifyRoute)
 
 export default router
