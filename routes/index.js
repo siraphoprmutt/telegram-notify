ecb73935db9505189abb8ee52c675f961b71ad63
siraphop-oLe <siraphop.rmutt@gmail.com>
2025-04-03 23:25:41 +0700

    up

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,16 @@
 import { Router } from "express"
-import fs from "fs"
-import path from "path"
+import notifyRoute from "./notify.js"
 
 const router = Router()
-const __dirname = path.resolve()
 
-const routesPath = path.join(__dirname, "routes")
-
-fs.readdirSync(routesPath).forEach(async (file) => {
-    const fileName = path.parse(file).name
-    const fileExt = path.parse(file).ext
-
-    if (fileName === "index" || fileExt !== ".js") return
-
-    const route = await import(`./${file}`)
-    router.use(`/${fileName}`, route.default)
-})
-
-// Root route
+// root route
 router.get("/", (req, res) => {
     res.status(200).json({
         message: "Welcome to the Telegram Bot API",
     })
 })
+
+// other routes
+router.use("/notify", notifyRoute)
 
 export default router
